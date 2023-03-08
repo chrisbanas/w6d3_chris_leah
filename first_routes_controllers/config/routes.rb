88bd889190b64363af7d309411667d78db1d9ca9
chrisbanas <bananas595@gmail.com>
2023-03-08 15:05:06 -0800

    On phase 2

--- first_routes_controllers/config/routes.rb
+++ first_routes_controllers/config/routes.rb
@@ -3,4 +3,26 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # resources :users
+
+
+# # VERB '/path', to: 'controller#action'
+# get '/chrips', to: 'chirps#index'
+
+# # create a custom route
+# get '/chirps', to: 'chirps#search'
+
+
+
+  get 'users', to: 'users#index', as: 'users'
+  post 'users', to: 'users#create'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  patch "/users/:id", to: "users#update"
+  put "/users/:id", to: "users#update"
+  delete "/users/:id", to: "users#destroy"
+
+
 end
